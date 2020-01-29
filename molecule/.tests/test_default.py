657e42e4bfed7b704ec6c26d6676dcca731c2dc4
Bernhard Strähle <bernhard.straehle@gmail.com>
2020-01-29 01:10:14 +0100

    implemented python 2/3 detection and added support for centos 8

--- molecule/.tests/test_default.py
+++ molecule/.tests/test_default.py
@@ -8,4 +8,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_python(host):
     python_version = host.run("python -V")
+    if python_version.rc != 0:
+        python_version = host.run("python2 -V")
+        if python_version.rc != 0:
+            python_version = host.run("python3 -V")
+
     assert python_version.rc == 0
